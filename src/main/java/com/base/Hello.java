ae78ab37617914aa6c742b6f815737dff710f8b7
sanketgaikwad2001 <sanket.gaikwad6498@gmail.com>
2025-12-21 14:10:20 +0530

    Fibonacci code added

--- src/main/java/com/base/Hello.java
+++ src/main/java/com/base/Hello.java
@@ -6,5 +6,17 @@ public class Hello {
 
 		System.out.println("Hello, Satyawan....");
 		System.out.println(10 + 10);
+
+		int num = 10;
+
+		int num1 = 0;
+		int num2 = 1;
+
+		while (num >= num1) {
+			System.out.println(num1);
+			int num3 = num1 + num2;
+			num1 = num2;
+			num2 = num3;
+		}
 	}
 }
